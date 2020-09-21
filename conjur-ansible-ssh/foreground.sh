28e6bb3cd5a5c3fc2f461e803377fc399dcb9549
Quincy Cheng <quincy.cheng@gmail.com>
2020-09-21 18:36:23 +0800

    Update foreground.sh

--- conjur-ansible-ssh/foreground.sh
+++ conjur-ansible-ssh/foreground.sh
@@ -1,2 +1,2 @@
-git clone https://github.com/quincycheng/katacoda-env-conjur-ansible-ssh.git &> /dev/null
-cd katacoda-env-conjur-ansible-ssh &> /dev/null
+git clone https://github.com/quincycheng/katacoda-env-conjur-ansible-ssh.git 
+cd katacoda-env-conjur-ansible-ssh
