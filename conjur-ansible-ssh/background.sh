4bc1fef12131f76170ede54d76fe72142273c0e2
Quincy Cheng <quincy.cheng@gmail.com>
2020-09-21 18:58:09 +0800

    Update background.sh

--- conjur-ansible-ssh/background.sh
+++ conjur-ansible-ssh/background.sh
@@ -1,4 +1,4 @@
 rm * -rf 
 git clone https://github.com/quincycheng/katacoda-env-conjur-ansible-ssh.git 
-mv katacoda-env-conjur-ansible-ssh\* .
+mv katacoda-env-conjur-ansible-ssh/* .
 chmod +x conjur/setupConjur.sh
