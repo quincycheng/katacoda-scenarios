6ab010df1fab79110ed80ee6dfe6168e19e7bc81
Quincy Cheng <quincy.cheng@gmail.com>
2020-09-23 17:36:53 +0800

    Update 1_pull_everything.sh

--- environments/conjur-jenkins/build/1_pull_everything.sh
+++ environments/conjur-jenkins/build/1_pull_everything.sh
@@ -6,6 +6,7 @@ docker pull jenkins/jenkins:lts
 docker pull postgres:9.3
 docker pull conjurinc/cli5
 docker pull cyberark/conjur
+docker pull python:2.7
 
 cd /tmp
 git clone https://github.com/quincycheng/katacoda-env-conjur-jenkins.git
