414e9f9f932a94beb299f5009fc46fd20496163a
Quincy Cheng <quincy.cheng@gmail.com>
2020-10-17 15:33:17 +0800

    Update and rename info-fg.sh to intro-fg.sh

--- conjur-k8s-secretless/intro-fg.sh
+++ conjur-k8s-secretless/intro-fg.sh
@@ -1,5 +1,6 @@
-#!/bin/bash +x
-
+#!/bin/bash -ex
+{ set +x; } 2>/dev/null
 git clone https://github.com/quincycheng/katacoda-secretless-files.git > /dev/null 2>&1
 /usr/bin/launch.sh
 cd katacoda-secretless-files/ 
+set -x; 
