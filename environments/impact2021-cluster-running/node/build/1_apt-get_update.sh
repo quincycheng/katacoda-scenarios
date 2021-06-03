6da65fa02d375a9205a47680a578ec1505a89ff7
Quincy Cheng <quincy.cheng@gmail.com>
2021-06-03 14:38:24 +0800

    Update 1_apt-get_update.sh

--- environments/impact2021-cluster-running/node/build/1_apt-get_update.sh
+++ environments/impact2021-cluster-running/node/build/1_apt-get_update.sh
@@ -1,12 +1,14 @@
 #System Update
 
-apt-get update
 yes | apt-get upgrade
 
+
+# 
+
 # add service02 account
-useradd -m -d /tmp service02
-echo -e "5;LF+J4Rfqds:DZ8\n5;LF+J4Rfqds:DZ8\n" | passwd service02
+#useradd -m -d /tmp service02
+#echo -e "5;LF+J4Rfqds:DZ8\n5;LF+J4Rfqds:DZ8\n" | passwd service02
 
 # Add service01 a/c
-useradd -m -d /tmp service01
-echo 'service02:5;LF+J4Rfqds:DZ8' | chpasswd
+#useradd -m -d /tmp service01
+#echo 'service02:5;LF+J4Rfqds:DZ8' | chpasswd
