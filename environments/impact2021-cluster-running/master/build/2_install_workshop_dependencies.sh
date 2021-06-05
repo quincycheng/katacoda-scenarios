fe930397c5e61b860fc6bc3e2f77c940eb554e13
Quincy Cheng <quincy.cheng@gmail.com>
2021-06-05 17:26:01 +0800

    Update 2_install_workshop_dependencies.sh

--- environments/impact2021-cluster-running/master/build/2_install_workshop_dependencies.sh
+++ environments/impact2021-cluster-running/master/build/2_install_workshop_dependencies.sh
@@ -11,17 +11,26 @@ docker run --name lamp -d -p "80:80" -v ${PWD}/app:/app mattrayner/lamp:latest-1
 docker exec lamp mysql -h localhost -uroot \
     -e "CREATE DATABASE conjur_demo; CREATE USER 'devapp1' IDENTIFIED BY 'Cyberark1'; GRANT ALL PRIVILEGES ON conjur_demo.* TO 'devapp1'; FLUSH PRIVILEGES; CREATE TABLE IF NOT EXISTS conjur_demo.demo (message VARCHAR(255) NOT NULL) ENGINE=MyISAM DEFAULT CHARSET=utf8; INSERT INTO demo (message) VALUES ('If you are seeing this message, we have successfully connected PHP to our backend MySQL database!');"
 
+# Get Policy files
+mkdir /tmp/infamousjoeg
+cd /tmp/infamousjoeg
+git clone https://github.com/infamousjoeg/katacoda-scenarios.git
+mkdir /tmp/policies/
+cp katacoda-scenarios/impact2021-security-workshop_webapp/assets/* /tmp/policies
+
+
 #move to /root
 cd /root
 
 # Install Conjur OSS quick-start
-curl -fsSL cybr.rocks/conjur-install | bash -s
+#curl -fsSL cybr.rocks/conjur-install | bash -s
+curl -fsSL cybr.rocks/conjur-install -o conjur-install.sh && ./conjur-install.sh --no-update
 
 echo "done" >> /root/katacoda-finished
 
 
 # Copy & load root.yml policy file
-docker cp /policies/* root_client_1:/policies/
+docker cp /tmp/policies/* root_client_1:/policies/
 # Output API key to /root/demouser.txt
 docker exec root_client_1 conjur policy load root /policies/root.yml > demouser.txt
 
