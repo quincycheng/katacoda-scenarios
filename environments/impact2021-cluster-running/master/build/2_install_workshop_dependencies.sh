83bad290365c5e6ac9a46e52185e0fa3aad05993
Quincy Cheng <quincy.cheng@gmail.com>
2021-06-06 16:39:10 +0800

    Update 2_install_workshop_dependencies.sh

--- environments/impact2021-cluster-running/master/build/2_install_workshop_dependencies.sh
+++ environments/impact2021-cluster-running/master/build/2_install_workshop_dependencies.sh
@@ -8,18 +8,18 @@ docker pull mattrayner/lamp:latest-1804
 
 # Deploy LAMP & create MySQL database and user
 docker run --name lamp -d -p "80:80" -p "3306:3306" -v ${PWD}/app:/app -v ${PWD}/mysql:/var/lib/mysql mattrayner/lamp:latest-1804
+sleep 1m 
 docker exec lamp mysql -h localhost --port=3306 -uroot \
     -e "CREATE DATABASE conjur_demo; CREATE USER 'devapp1' IDENTIFIED BY 'Cyberark1'; GRANT ALL PRIVILEGES ON conjur_demo.* TO 'devapp1'; FLUSH PRIVILEGES; CREATE TABLE IF NOT EXISTS conjur_demo.demo (message VARCHAR(255) NOT NULL) ENGINE=MyISAM DEFAULT CHARSET=utf8; INSERT INTO demo (message) VALUES ('If you are seeing this message, we have successfully connected PHP to our backend MySQL database!');"
 
 # Get Policy files
-mkdir /tmp/infamousjoeg
-cd /tmp/infamousjoeg
+cd /tmp
 git clone https://github.com/infamousjoeg/katacoda-scenarios.git
 mkdir /tmp/policies/
 cp katacoda-scenarios/impact2021-security-workshop_webapp/assets/* /tmp/policies
 
 
-#move to /root
+# Move to /root
 cd /root
 
 # Install Conjur OSS quick-start
@@ -32,7 +32,7 @@ echo "done" >> /root/katacoda-finished
 
 
 # Copy & load root.yml policy file
-docker cp /tmp/policies/* root_client_1:/policies/
+docker cp /tmp/policies/root.yml root_client_1:/policies/
 # Output API key to /root/demouser.txt
 docker exec root_client_1 conjur policy load root /policies/root.yml > demouser.txt
 
@@ -44,3 +44,14 @@ docker exec root_client_1 conjur variable values add devapp/db_uname devapp1
 docker exec root_client_1 conjur variable values add devapp/db_pass Cyberark1
 
 echo "done" >> /root/katacoda-background-finished
+
+
+# script will be run when the session starts 
+cat << EOF > /opt/configure-environment.sh
+#!/bin/bash
+docker start root_database_1
+docker start root_client_1
+docker start lamp
+docker start root_conjur_1
+EOF
+chmod +x /opt/configure-environment.sh
