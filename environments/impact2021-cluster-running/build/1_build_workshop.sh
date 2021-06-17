84f585478e2dce76afdf6d56c753ae1477569138
Joe Garcia <joe@joe-garcia.com>
2021-06-17 16:56:05 -0400

    combine build script

--- environments/impact2021-cluster-running/build/1_build_workshop.sh
+++ environments/impact2021-cluster-running/build/1_build_workshop.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# Update apt repos and install deps
+#Ansible
 apt-get update
+apt-add-repository --yes --update ppa:ansible/ansible
+apt-get update
+apt-get install ansible sshpass -y
+apt-get install -y wamerican
+
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+# Update apt repos and install deps
 apt-get install -y mysql-client-core-5.7 jq
 
 
@@ -33,31 +41,23 @@ curl -fsSL cybr.rocks/conjur-install -o conjur-install.sh
 chmod +x ./conjur-install.sh
 ./conjur-install.sh --no-update
 
-echo "done" >> /root/katacoda-finished
-
-
 # Copy & load root.yml policy file
 docker exec root_client_1 mkdir /policies/
 docker cp /opt/policies/root.yml root_client_1:/policies/
+
 # Output API key to /root/demouser.txt
 docker exec root_client_1 conjur policy load root /policies/root.yml > demouser.txt
-
-# Add alias for Conjur CLI to .bashrc
-echo "alias conjur='docker exec root_client_1 conjur'" >> /root/.bashrc
 
 # Initialize devapp secret variables
 docker exec root_client_1 conjur variable values add devapp/db_uname devapp1
 docker exec root_client_1 conjur variable values add devapp/db_pass Cyberark1
-
-echo "done" >> /root/katacoda-background-finished
-
 
 # script will be run when the session starts 
 cat << EOF > /opt/configure-environment.sh
 #!/bin/bash
 docker start root_database_1
 docker start lamp
-docker start root_client_1
 docker start root_conjur_1
+docker start root_client_1
 EOF
 chmod +x /opt/configure-environment.sh
