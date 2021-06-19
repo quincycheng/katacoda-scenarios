c14d171d9822fb6d5d1e08253e628799dde4a774
Joe Garcia <joe@joe-garcia.com>
2021-06-19 13:03:22 -0400

    fix lamp not receiving env vars

--- impact2021-security-workshop_webapp/background.sh
+++ impact2021-security-workshop_webapp/background.sh
@@ -5,7 +5,26 @@ docker start root_client_1
 docker start lamp
 sleep 3s
 docker start root_conjur_1
+
+echo "done" >> /root/katacoda-finished
+
+##### THE FOLLOWING SHOULD BE MOVED TO THE ENVIRONMENT BUILD #####
 CONJUR_AUTHN_LOGIN=$(docker exec root_client_1 printenv CONJUR_AUTHN_LOGIN)
 CONJUR_APPLIANCE_URL=$(docker exec root_client_1 printenv CONJUR_APPLIANCE_URL)
 CONJUR_AUTHN_API_KEY=$(docker exec root_client_1 awk 'NR==3 {print $NF}' /root/.netrc)
-docker exec lamp bash -c 'export CONJUR_AUTHN_LOGIN=$CONJUR_AUTHN_LOGIN; export CONJUR_APPLIANCE_URL=$CONJUR_APPLIANCE_URL; export CONJUR_AUTHN_API_KEY=$CONJUR_AUTHN_API_KEY'
+
+docker rm -f lamp
+docker run --name lamp -d -p "80:80" -p "3306:3306" \
+    -e CONJUR_AUTHN_LOGIN="$CONJUR_AUTHN_LOGIN" \
+    -e CONJUR_AUTHN_API_KEY="$CONJUR_AUTHN_API_KEY" \
+    -e CONJUR_APPLIANCE_URL="$CONJUR_APPLIANCE_URL" \
+    -v /opt/app:/app -v /opt/mysql:/var/lib/mysql \
+    mattrayner/lamp:latest-1804
+# sleep for 1 min to make sure MySQL is up & running
+sleep 1m 
+# Add "use conjur_demo"
+docker exec lamp mysql -h localhost --port=3306 -uroot \
+    -e "CREATE DATABASE conjur_demo;  CREATE USER 'devapp1' IDENTIFIED BY 'Cyberark1'; GRANT ALL PRIVILEGES ON conjur_demo.* TO 'devapp1'; FLUSH PRIVILEGES; USE conjur_demo; CREATE TABLE IF NOT EXISTS conjur_demo.demo (message VARCHAR(255) NOT NULL) ENGINE=MyISAM DEFAULT CHARSET=utf8; INSERT INTO demo (message) VALUES ('If you are seeing this message, we have successfully connected PHP to our backend MySQL database!');"
+##################################################################
+
+echo "done" >> /root/katacoda-background-finished
--- impact2021-security-workshop_webapp/foreground.sh
+++ impact2021-security-workshop_webapp/foreground.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
 
+# Wait for all containers to start and configure
+while [ ! -f /usr/local/bin/wait.sh ]; do sleep 1; done; /usr/local/bin/wait.sh
