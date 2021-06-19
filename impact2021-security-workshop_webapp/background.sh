6ec53c203523b460475dc58db42359f4457335cf
Joe Garcia <joe@joe-garcia.com>
2021-06-19 12:42:11 -0400

    Update background tasks

--- impact2021-security-workshop_webapp/background.sh
+++ impact2021-security-workshop_webapp/background.sh
@@ -5,3 +5,7 @@ docker start root_client_1
 docker start lamp
 sleep 3s
 docker start root_conjur_1
+CONJUR_AUTHN_LOGIN=$(docker exec root_client_1 printenv CONJUR_AUTHN_LOGIN)
+CONJUR_APPLIANCE_URL=$(docker exec root_client_1 printenv CONJUR_APPLIANCE_URL)
+CONJUR_AUTHN_API_KEY=$(docker exec root_client_1 awk 'NR==3 {print $NF}' /root/.netrc)
+docker exec lamp bash -c 'export CONJUR_AUTHN_LOGIN=$CONJUR_AUTHN_LOGIN; export CONJUR_APPLIANCE_URL=$CONJUR_APPLIANCE_URL; export CONJUR_AUTHN_API_KEY=$CONJUR_AUTHN_API_KEY'
