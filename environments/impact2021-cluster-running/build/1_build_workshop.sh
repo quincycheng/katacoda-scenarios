f52774ab5b4617db35780972359334c857dfec1b
Joe Garcia <joe@joe-garcia.com>
2021-06-24 13:03:32 -0400

    sleep for 1m

--- environments/impact2021-cluster-running/build/1_build_workshop.sh
+++ environments/impact2021-cluster-running/build/1_build_workshop.sh
@@ -61,7 +61,7 @@ docker run --name lamp -d -p "80:80" -p "3306:3306" \
     -v /opt/app:/app -v /opt/mysql:/var/lib/mysql \
     mattrayner/lamp:latest-1804
 
-sleep 15s
+sleep 1m
 # Add "use conjur_demo"
 docker exec lamp mysql -h localhost --port=3306 -uroot \
     -e "CREATE DATABASE conjur_demo;  CREATE USER 'devapp1' IDENTIFIED BY 'Cyberark1'; GRANT ALL PRIVILEGES ON conjur_demo.* TO 'devapp1'; FLUSH PRIVILEGES; USE conjur_demo; CREATE TABLE IF NOT EXISTS conjur_demo.demo (message VARCHAR(255) NOT NULL) ENGINE=MyISAM DEFAULT CHARSET=utf8; INSERT INTO demo (message) VALUES ('If you are seeing this message, we have successfully connected PHP to our backend MySQL database!');"
