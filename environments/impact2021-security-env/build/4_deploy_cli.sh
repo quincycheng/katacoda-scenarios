6ebacbb24e3436d395a51cf67905cc5c58c27f6d
Joe Garcia <joe@joe-garcia.com>
2021-06-25 10:33:36 -0400

    keep cli container running

--- environments/impact2021-security-env/build/4_deploy_cli.sh
+++ environments/impact2021-security-env/build/4_deploy_cli.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 # Download the cybr-cli
-docker run --name cybr-cli -d nfmsjoeg/cybr-cli:0.1.3-beta
+docker run --name cybr-cli -d --network root_default --entrypoint sleep nfmsjoeg/cybr-cli:0.1.3-beta infinity
 docker cp cybr-cli:/app/cybr /usr/local/bin
-docker rm -f cybr-cli
