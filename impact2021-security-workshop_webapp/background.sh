7de9146996ad8ac970e3c33af1d29cd25aed2fb4
Joe Garcia <joe@joe-garcia.com>
2021-06-25 11:55:11 -0400

    start cli container

--- impact2021-security-workshop_webapp/background.sh
+++ impact2021-security-workshop_webapp/background.sh
@@ -3,8 +3,12 @@ docker start root_database_1
 sleep 3s
 docker start lamp
 sleep 3s
+docker start root_client_1
+sleep 3s
 docker start root_conjur_1
 
 echo "done" >> /root/katacoda-finished
+
+docker rm -f cybr-cli
 
 echo "done" >> /root/katacoda-background-finished
