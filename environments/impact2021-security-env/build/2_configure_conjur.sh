847e9724e6772be0266b2a7217acbc9be3b43f4a
Joe Garcia <joe@joe-garcia.com>
2021-06-25 14:44:31 -0400

    start conjur unconfigured but with cli

--- environments/impact2021-security-env/build/2_configure_conjur.sh
+++ environments/impact2021-security-env/build/2_configure_conjur.sh
@@ -1,25 +1,12 @@
 #!/bin/bash
 
-# Deploy & Configure Conjur CLI
-docker run --name cli -d --restart always --network conjur \
-    -v /root/policy:/policy \
+# Download the cybr-cli
+docker run --name cybr-cli -d --network conjur \
     --entrypoint sleep \
-    cyberark/conjur-cli:5-latest infinity
-docker exec -i cli conjur init --account impact2021 --url https://conjur <<< yes
-docker exec cli conjur authn login -u admin -p CYberark11@@
+    nfmsjoeg/cybr-cli:0.1.3-beta infinity
+docker cp cybr-cli:/app/cybr /usr/local/bin
 
 # Get Policy files
+mkdir /root/policies
 git clone https://github.com/infamousjoeg/katacoda-scenarios.git
-mkdir /opt/policies/
-cp katacoda-scenarios/impact2021-security-workshop_webapp/assets/* /opt/policies
-
-# Copy & load root.yml policy file
-docker exec cli mkdir /policies/
-docker cp /opt/policies/root.yml cli:/policies/
-
-# Output API key to /root/demouser.txt
-docker exec cli conjur policy load root /policies/root.yml > demouser.txt
-
-# Initialize devapp secret variables
-docker exec cli conjur variable values add devapp/db_uname devapp1
-docker exec cli conjur variable values add devapp/db_pass Cyberark1
+cp katacoda-scenarios/impact2021-security-workshop_webapp/assets/root.yml /root/policies/root.yml
