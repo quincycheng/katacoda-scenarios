bc8710ac5e37e7bceb63a186e0f91fbdfda39f79
Quincy Cheng <quincy.cheng@gmail.com>
2021-06-28 15:19:08 +0800

    Update 1_build_workshop.sh

--- environments/impact2021-cluster-running/build/1_build_workshop.sh
+++ environments/impact2021-cluster-running/build/1_build_workshop.sh
@@ -8,11 +8,54 @@ apt-add-repository --yes --update ppa:ansible/ansible
 apt-get update
 apt-get install ansible sshpass -y
 apt-get install -y wamerican
-
 export ANSIBLE_HOST_KEY_CHECKING=False
 
-# Update apt repos and install deps
+# MySQL Client
 apt-get install -y mysql-client-core-5.7 jq
+
+
+
+
+# Install Conjur OSS quick-start
+cat <<EOF > docker-compose.yml 
+version: '3'
+services:
+  database:
+    image: postgres:9.3
+
+  conjur:
+    image: cyberark/conjur
+    command: server
+    environment:
+      DATABASE_URL: postgres://postgres@database/postgres
+      CONJUR_DATA_KEY:
+    depends_on: [ database ]
+    ports:
+      - "8080:80"
+    volumes:
+      - /root/postgres-data:/var/lib/postgresql/data
+    
+  client:
+    image: conjurinc/cli5
+    depends_on: [ conjur ]
+    entrypoint: sleep
+    command: infinity
+    environment:
+      CONJUR_APPLIANCE_URL: http://conjur
+      CONJUR_ACCOUNT:
+      CONJUR_AUTHN_API_KEY:
+      CONJUR_AUTHN_LOGIN: admin
+EOF
+mkdir /root/postgres-data
+docker-compose pull
+docker-compose run --no-deps --rm conjur data-key generate > /root/data_key
+export CONJUR_DATA_KEY="$(< data_key)"
+docker-compose up -d 
+
+# cybr-cli
+docker run --name cybr-cli -d nfmsjoeg/cybr-cli:0.1.3-beta
+docker cp cybr-cli:/app/cybr /usr/local/bin
+docker rm -f cybr-cli
 
 
 # Deploy LAMP & create MySQL database and user
@@ -28,21 +71,12 @@ mkdir /opt/policies/
 cp katacoda-scenarios/impact2021-security-workshop_webapp/assets/* /opt/policies
 
 
-# Move to /root
-cd /root
-
-# Install Conjur OSS quick-start
-#curl -fsSL cybr.rocks/conjur-install | bash -s
-curl -fsSL cybr.rocks/conjur-install -o conjur-install.sh 
-chmod +x ./conjur-install.sh
-./conjur-install.sh --no-update
-
 # Copy & load root.yml policy file
 docker exec root_client_1 mkdir /policies/
 docker cp /opt/policies/root.yml root_client_1:/policies/
 
 # Output API key to /root/demouser.txt
-docker exec root_client_1 conjur policy load root /policies/root.yml > demouser.txt
+docker exec root_client_1 conjur policy load root /policies/root.yml > /root/demouser.txt
 
 # Initialize devapp secret variables
 docker exec root_client_1 conjur variable values add devapp/db_uname devapp1
@@ -54,7 +88,8 @@ CONJUR_APPLIANCE_URL=$(docker exec root_client_1 printenv CONJUR_APPLIANCE_URL)
 CONJUR_AUTHN_API_KEY=$(docker exec root_client_1 awk 'NR==3 {print $NF}' /root/.netrc)
 
 # Remove old CLI container
-docker rm -f root_client_1
+#docker rm -f root_client_1
+
 # Start LAMP container with host identity
 docker run --name lamp -d -p "80:80" -p "3306:3306" \
     -e CONJUR_AUTHN_LOGIN="$CONJUR_AUTHN_LOGIN" \
@@ -68,9 +103,5 @@ sleep 1m
 docker exec lamp mysql -h localhost --port=3306 -uroot \
     -e "CREATE DATABASE conjur_demo;  CREATE USER 'devapp1' IDENTIFIED BY 'Cyberark1'; GRANT ALL PRIVILEGES ON conjur_demo.* TO 'devapp1'; FLUSH PRIVILEGES; USE conjur_demo; CREATE TABLE IF NOT EXISTS conjur_demo.demo (message VARCHAR(255) NOT NULL) ENGINE=MyISAM DEFAULT CHARSET=utf8; INSERT INTO demo (message) VALUES ('If you are seeing this message, we have successfully connected PHP to our backend MySQL database!');"
 
-# Download the cybr-cli
-docker run --name cybr-cli -d nfmsjoeg/cybr-cli:0.1.3-beta
-docker cp cybr-cli:/app/cybr /usr/local/bin
-docker rm -f cybr-cli
 
 echo "End of impact2021-cluster-running env"
