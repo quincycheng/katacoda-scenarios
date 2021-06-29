f436b5089fb2d84cc9fd5069f3b160e3dc29a5b9
Quincy Cheng <quincy.cheng@gmail.com>
2021-06-29 14:27:13 +0800

    Update background.sh

--- impact2021-security-workshop_playground/background.sh
+++ impact2021-security-workshop_playground/background.sh
@@ -8,5 +8,7 @@ docker rm root_conjur_1
 cd /root
 docker-compose up -d
 
-echo "done" >> /root/katacoda-finished
-echo "done" >> /root/katacoda-background-finished
+/opt/vscode/node /opt/vscode/out/node/entry.js --host=0.0.0.0 --port=23000 --disable-ssh --auth=none --disable-telemetry --disable-updates --extensions-dir=/opt/.katacodacode/extensions/ --user-data-dir=/opt/.katacodacode/user-data &
+
+#echo "done" >> /root/katacoda-finished
+#echo "done" >> /root/katacoda-background-finished
