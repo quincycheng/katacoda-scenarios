79006ffa59c0127a7f2e2786f20e1a6e0f8066e1
Quincy Cheng <quincy.cheng@gmail.com>
2021-06-30 11:19:00 +0800

    Update foreground.sh

--- impact2021-security-workshop_playground/foreground.sh
+++ impact2021-security-workshop_playground/foreground.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-clear&&printf "Verifying environment..."&&while ! docker exec root_client_1 conjur list >/dev/null 2>&1;do printf ".";sleep 1s;done && echo -e "\nReady!"
+clear && printf "Verifying environment..." && timeout 90s bash -c 'while ! docker exec root_client_1 conjur list >/dev/null 2>&1;do printf ".";sleep 1s;done' && echo -e "\nReady!"
