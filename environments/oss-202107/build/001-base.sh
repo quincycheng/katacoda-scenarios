9f9b9454a2c23b697a1c9380f1d2c54b8739b549
Quincy Cheng <quincy.cheng@gmail.com>
2021-07-05 17:04:33 +0800

    Update 001-base.sh

--- environments/oss-202107/build/001-base.sh
+++ environments/oss-202107/build/001-base.sh
@@ -106,10 +106,16 @@ docker exec root_client_1 conjur policy load root /policies/root.yml > /root/dem
 docker exec root_client_1 conjur variable values add devapp/db_uname devapp1
 docker exec root_client_1 conjur variable values add devapp/db_pass Cyberark1
 
-# Grab Conjur authn info
-CONJUR_AUTHN_LOGIN=$(docker exec root_client_1 printenv CONJUR_AUTHN_LOGIN)
+# Grab Conjur authn info (admin)
+#CONJUR_AUTHN_LOGIN=$(docker exec root_client_1 printenv CONJUR_AUTHN_LOGIN)
+#CONJUR_APPLIANCE_URL=$(docker exec root_client_1 printenv CONJUR_APPLIANCE_URL)
+#CONJUR_AUTHN_API_KEY=$(docker exec root_client_1 awk 'NR==3 {print $NF}' /root/.netrc)
+
+# Grab Conjur authn info (host identity)
 CONJUR_APPLIANCE_URL=$(docker exec root_client_1 printenv CONJUR_APPLIANCE_URL)
-CONJUR_AUTHN_API_KEY=$(docker exec root_client_1 awk 'NR==3 {print $NF}' /root/.netrc)
+CONJUR_AUTHN_LOGIN=$(cat demouser.txt | jq -r '.created_roles' | jq -r '.[].id')
+CONJUR_AUTHN_API_KEY=$(cat demouser.txt | jq -r '.created_roles' | jq -r '.[].api_key')
+
 
 # Remove old CLI container
 #docker rm -f root_client_1
@@ -122,10 +128,14 @@ docker run --name lamp -d -p "80:80" -p "3306:3306" \
     -v /opt/app:/app -v /opt/mysql:/var/lib/mysql \
     mattrayner/lamp:latest-1804
 
+# Connect LAMP to Conjur network
+docker network connect root_default lamp
+
 sleep 1m
 # Add "use conjur_demo"
 docker exec lamp mysql -h localhost --port=3306 -uroot \
     -e "CREATE DATABASE conjur_demo;  CREATE USER 'devapp1' IDENTIFIED BY 'Cyberark1'; GRANT ALL PRIVILEGES ON conjur_demo.* TO 'devapp1'; FLUSH PRIVILEGES; USE conjur_demo; CREATE TABLE IF NOT EXISTS conjur_demo.demo (message VARCHAR(255) NOT NULL) ENGINE=MyISAM DEFAULT CHARSET=utf8; INSERT INTO demo (message) VALUES ('If you are seeing this message, we have successfully connected PHP to our backend MySQL database!');"
+
 
 touch /opt/conjur/is_ready.sh
 cat <<EOF > /opt/conjur/is_ready.sh
