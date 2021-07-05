545f80f2465f61fa96b4d96cb94aa5bbf714df7f
Quincy Cheng <quincy.cheng@gmail.com>
2021-07-05 17:28:23 +0800

    Update foreground.sh

--- impact2021-security-workshop_conjur/foreground.sh
+++ impact2021-security-workshop_conjur/foreground.sh
@@ -1,2 +1,8 @@
-set +H
-clear && printf "Verifying environment..." && docker kill $(docker ps -aq) && docker rm $(docker ps -aq)&& echo -e "\nReady!"
+#!/bin/bash
+clear && printf "Clearing environment..." 
+docker kill $(docker ps -aq) 
+docker rm $(docker ps -aq)
+rm /root/docker-compose.yml
+rm /root/admin.out
+rm /root/demouser.txt
+clear && echo -e "\nReady!"
