c991cecd404ea4f2c5611ebab107434d5424acf5
Quincy Cheng <quincy.cheng@gmail.com>
2021-07-07 12:14:15 +0800

    Update foreground.sh

--- impact2021-security-workshop_ansible/foreground.sh
+++ impact2021-security-workshop_ansible/foreground.sh
@@ -1,1 +1,3 @@
-
+#!/bin/bash
+set +H
+clear && printf "Verifying environment..." && sleep 1s && timeout 30s bash -c 'while ! [ -f /root/insecure-playbook/insecure-playbook.yml ];do printf ".";sleep 1s;done' && echo -e "\nReady!"
